e4c74bd217448c3a880516c4a4d7285c0d7e3255
Kostas Tsolis <kostas.tsolis@zoho.com>
2024-02-04 03:45:55 +0100

    fix custom header

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import * as React from "react";
-import { Alert, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  Alert,
+  Image,
+  ScrollView,
+} from "react-native";
 import Onboarding from "./screens/Onboarding";
 import ProfileScreen from "./screens/Profile";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -10,6 +19,24 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import AppStateContext from "./AppContext";
 
 const Stack = createNativeStackNavigator();
+
+function CustomHeader(props) {
+  console.log(props);
+  return (
+    <View style={styles.flexRow}>
+      <Text>Back</Text>
+      <Text style={styles.title}>{props.children}</Text>
+      <View style={styles.avatarContainer}>
+        {props.avatarUri && (
+          <Image source={{ uri: props.avatarUri }} style={styles.avatarImg} />
+        )}
+        {!props.avatarUri && (
+          <Text style={styles.avatarText}>{props.avatarInitials}</Text>
+        )}
+      </View>
+    </View>
+  );
+}
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -36,32 +63,25 @@ export default function App() {
   };
   const loadLocalState = async () => {
     try {
-      const savedState = await AsyncStorage.getItem('appState')
+      const savedState = await AsyncStorage.getItem("appState");
       if (savedState !== null) {
-        let parsedState = JSON.parse(savedState)
-        console.log('parsed local state: '+ JSON.stringify(parsedState))
-        return parsedState
+        let parsedState = JSON.parse(savedState);
+        console.log("parsed local state: " + JSON.stringify(parsedState));
+        return parsedState;
       } else {
-        return defaultState
+        return defaultState;
       }
     } catch (e) {
-      return defaultState
+      return defaultState;
     }
-  }
+  };
 
   useEffect(() => {
     console.log("initial state: " + JSON.stringify(state));
     loadLocalState().then((loadedState) => {
-      setState({...state, ...loadedState, isLoading: false})
-        console.log("initially loaded state: " + JSON.stringify(state));
-
-    })
-    // getLocalOnboardingStatus().then((storedLoginStatus) => {
-    //   setState({ ...state, isLoading: false });
-    //   console.log("initially loaded state: " + JSON.stringify(state));
-    //   console.log("onLoad storedLoginStatus: " + storedLoginStatus);
-    //   console.log("---\n");
-    // });
+      setState({ ...state, ...loadedState, isLoading: false });
+      console.log("initially loaded state: " + JSON.stringify(state));
+    });
   }, []);
 
   useEffect(() => {
@@ -105,7 +125,14 @@ export default function App() {
         <Stack.Navigator>
           {state.isOnboardingCompleted ? (
             // onboarding completed, user is signed in
-            <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen
+              name="Profile"
+              component={ProfileScreen}
+              options={{
+                title: "Personal Info",
+                headerTitle: (props) => <CustomHeader {...props} {...state} />,
+              }}
+            />
           ) : (
             // user is not singed in
             <Stack.Screen name="Onboarding" component={Onboarding} />
@@ -115,3 +142,37 @@ export default function App() {
     </AppStateContext.Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  flexRow: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 10,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  avatarContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#47733e",
+    marginRight: 20,
+  },
+  avatarText: {
+    textAlign: "center",
+    color: "#e5e3e3",
+    fontWeight: "600",
+    fontSize: 10,
+  },
+  avatarImg: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+});
