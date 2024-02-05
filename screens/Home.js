7ffb8384997f45467b6161b3ff865891ba6a6759
Kostas Tsolis <kostas.tsolis@zoho.com>
2024-02-05 15:41:08 +0100

    stylize menu screen

--- screens/Home.js
+++ screens/Home.js
@@ -7,31 +7,68 @@ import {
   Pressable,
   Alert,
   Image,
-  ScrollView,
   FlatList,
 } from "react-native";
-import AppStateContext from "../AppContext";
+
+const ItemView = ({ item }) => {
+  function trimText(text, limit) {
+    if (text.length > limit) {
+      return text.substring(0, limit) + "...";
+    } else {
+      return text;
+    }
+  }
+  return (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemHeader}>{item.name}</Text>
+      <View style={styles.flexRow}>
+        <View style={styles.flexTextInfo}>
+          <Text style={styles.itemDescription}>
+            {trimText(item.description, 80)}
+          </Text>
+          <Text style={styles.itemPrice}>${item.price}</Text>
+        </View>
+        <Image source={require("../assets/icon.png")} style={styles.foodImg} />
+      </View>
+
+      {/*<Image source={{ uri: item.image }} style={styles.foodImg} />*/}
+    </View>
+  );
+};
 
 function HomeHeader() {
   return (
     <View>
-      <View>
-        <Text>Little Lemon</Text>
-        <Text>Chicago</Text>
-        <Text>
-          We are a family owned Mediterranean restaurant focused on traditional
-          recipes served with a modern twist.
-        </Text>
-        <Text>[Finder icon]</Text>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}>Little Lemon</Text>
+        <View style={styles.flexRow}>
+          <View style={styles.flexTextInfo}>
+            <Text style={styles.headerSubtitle}>Chicago</Text>
+            <Text style={styles.headerText}>
+              We are a family owned Mediterranean restaurant focused on
+              traditional recipes served with a modern twist.
+            </Text>
+          </View>
+          <Image
+            source={require("../assets/icon.png")}
+            style={styles.headerImg}
+          />
+        </View>
+
+        <Image
+          source={require("../assets/icon.png")}
+          style={styles.finderImg}
+        />
       </View>
-      <View>
-        <Text>Order for delivery</Text>
-        <Text>[Category filters]</Text>
+      <View style={styles.quickFilterContainer}>
+        <Text style={styles.quickFilterHeader}>Order for delivery</Text>
+        <Text style={styles.quickFilterButton}>[Category filters]</Text>
       </View>
     </View>
   );
 }
 export default function HomeScreen() {
+  // todo - fetch data https://www.coursera.org/learn/capstone-react-app/supplement/qCY26/exercise-building-the-home-screen
   const data = [
     {
       name: "Greek Salad",
@@ -47,6 +84,20 @@ export default function HomeScreen() {
         "Delicious grilled bread rubbed with garlic and topped with olive oil and salt. Our Bruschetta includes tomato and cheese.",
       image: "bruschetta.jpg",
     },
+    {
+      name: "Greek Salad",
+      price: 12.99,
+      description:
+        "Our delicious salad is served with Feta cheese and peeled cucumber. Includes tomatoes, onions, olives, salt and oregano in the ingredients.",
+      image: "greekSssalad.jpg",
+    },
+    {
+      name: "Bruschetta",
+      price: 7.99,
+      description:
+        "Delicious grilled bread rubbed with garlic and topped with olive oil and salt. Our Bruschetta includes tomato and cheese.",
+      image: "bruscheddtta.jpg",
+    },
   ];
   return (
     <View style={styles.container}>
@@ -60,34 +111,43 @@ export default function HomeScreen() {
   );
 }
 
-const ItemView = ({ item }) => {
-
-    function trimText(text, limit) {
-        if (text.length > limit) {
-            return text.substring(0, limit) + "..."
-        } else {
-            return text
-        }
-    }
-  return (
-    <View style={styles.itemContainer}>
-      <Text style={styles.itemHeader}>{item.name}</Text>
-      <View style={styles.flexRow}>
-        <View style={styles.flexTextInfo}>
-          <Text style={styles.itemDescription}>{trimText(item.description, 80)}</Text>
-          <Text style={styles.itemPrice}>${item.price}</Text>
-        </View>
-        <Image source={require("../assets/icon.png")} style={styles.foodImg} />
-      </View>
-
-      {/*<Image source={{ uri: item.image }} style={styles.foodImg} />*/}
-    </View>
-  );
-};
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  headerContainer: {
+    backgroundColor: "#095e2a",
+    padding: 10,
+  },
+  headerTitle: {
+    color: "#d9b628",
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  headerSubtitle: {
+    color: "#e0e6ea",
+    fontSize: 26,
+  },
+  headerText: {
+    color: "#e0e6ea",
+    fontSize: 14,
+  },
+  headerImg: {
+    width: 100,
+    height: 150,
+    flex: 1,
+  },
+  quickFilterContainer: {
+    padding: 10,
+  },
+  quickFilterHeader: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  quickFilterButton: {},
+  finderImg: {
+    width: 30,
+    height: 30,
   },
   foodImg: {
     width: 100,
